07d7252851bb5e811faacd834426b04ae053c059
Olivia M <olivia.martin.473@gmail.com>
2019-04-01 20:39:03 -0500

    forgot to add the .sql file for API data to be added to MySql database

---
+++ real_tech.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE real_tech_db;
+
+USE real_tech_db;
+
+CREATE TABLE business_licenses (
+	index_label INT AUTO_INCREMENT NOT NULL, 
+    legal_name VARCHAR(100),
+    doing_business_as_name VARCHAR(400),
+    zip_code VARCHAR(400),
+    license_description Integer,
+    business_activity Integer,
+    application_type VARCHAR(400),
+    license_start_date VARCHAR(20),
+    latitude Integer,
+    longitude Integer,
+    PRIMARY KEY (index_label)
+)
