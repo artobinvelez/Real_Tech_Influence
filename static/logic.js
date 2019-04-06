b2d1aaaa1d8e67e25e7194015597be1e9dc8fc74
Olivia M <olivia.martin.473@gmail.com>
2019-04-06 02:21:03 -0500

    updated hardcoding of geojson datat to include type: FeatureCollection, features, so we can now use L.geoJSON methods bc before data wasn't loading... updated logic to pull api now

--- static/logic.js
+++ static/logic.js
@@ -3,11 +3,9 @@
 // This gets inserted into the div with an id of 'map'
 // Creating map object
 var map = L.map("map", {
-  center: [41.8781, -87.6298],
-  zoom: 10
+  center: [41.907317, -87.659957],
+  zoom: 13
 });
-
-L.marker([-41.908, 87.634]).addTo(map);
 
 
 // Adding tile layer
@@ -25,25 +23,38 @@ var api_link = "http://127.0.0.1:5000/api"
 
 // Grabbing our GeoJSON data..
 d3.json(link, function(data) {
+  console.log(data)
   // Creating a GeoJSON layer with the retrieved data
   L.geoJson(data).addTo(map);
 });
 
-d3.json(api_link, function(response) {
+d3.json(api_link, function(data) {
+  L.geoJSON(data).addTo(map);
+})
 
-  // console.log(response);
+// headquarter markers
+var mcdIcon = L.icon({
+  iconUrl: 'static/Mcdonalds_logo.png',
 
-  for (var i = 0; i < response.length; i++) {
-    var location = response[i].geometry;
-    // console.log(location)
-    if (location) {
-      L.marker([location.coordinates[0], location.coordinates[1]]).addTo(map);
-      console.log(location.coordinates)
-    }
-  }
-
+  iconSize:     [70, 70], // size of the icon
 });
 
-// L.marker([41.903905986, -87.634626237]).addTo(map);
+L.marker([41.883486, -87.653842], {icon: mcdIcon}).addTo(map);
 
-// console.log()
+var groupIcon = L.icon({
+  iconUrl: 'static/groupon.png',
+
+  iconSize:     [50, 50], // size of the icon
+});
+
+L.marker([41.897692, -87.643751], {icon: groupIcon}).addTo(map);
+
+var coyIcon = L.icon({
+  iconUrl: 'static/Coyote.png',
+
+  iconSize:     [50, 50], // size of the icon
+});
+
+L.marker([41.931738, -87.692132], {icon: coyIcon}).addTo(map);
+
+// // console.log()
